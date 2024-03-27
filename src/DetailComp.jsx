c43a843b17a7f22f4600d97d4de7a77714d21db9
Nellie Gränd <nellie.grand@hotmail.com>
2024-03-27 12:27:06 +0100

    Some more code

--- src/DetailComp.jsx
+++ src/DetailComp.jsx
@@ -1,38 +1,44 @@
 import React, { useState, useEffect } from "react";
 import "./Styling/CompStyles.css";
-          
-function DetailComp({ mealId }) {
+
+//Skapar detaljkomponenten
+function DetailComp({ mealId }) { //mealId för att hålla koll på id bland maträtterna
     const [mealDetails, setMealDetails] = useState(null);
-  
+
+    // Ifall maträtten ändras
     useEffect(() => {
-      if (mealId) {
-        getDetails(mealId);
-      }
+        if (mealId) {
+            getDetails(mealId);
+        }
     }, [mealId]);
-  
+
     const getDetails = (id) => {
-      fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
-        .then((response) => response.json())
-        .then((result) => {
-          if (result.meals && result.meals.length > 0) {
-            setMealDetails(result.meals[0]);
-          } else {
-            setMealDetails(null);
-            console.error("Det finns inga detaljer om denna maträtt");
-          }
-        })
-        .catch((error) => {
-          console.error("Felmeddelande", error);
-        });
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+            .then((response) => response.json())
+            .then((result) => {
+                setMealDetails(result.meals[0]); //lägger till detaljerna på den klickade maträtten
+            })
+            .catch((error) => {
+                console.error("Felmeddelande", error); //fånga upp felmeddelande
+            });
     };
-  
+
     return (
-      <div>
-        {mealDetails && (
-          <p>{mealDetails.strInstructions}</p>
-        )}
-      </div>
+        <div>
+            {/* Visa detaljer om maträtterna om de finns */}
+            {mealDetails && (
+                <div>
+                <p>{mealDetails.idMeal}</p>
+                <p>{mealDetails.strMeal}</p>
+                <p>{mealDetails.strDrinkAlternate}</p>
+                <p>{mealDetails.strCategory}</p>
+                <p>{mealDetails.strArea}</p>
+                <p>{mealDetails.strInstructions}</p>
+                </div>
+            )}
+        </div>
     );
-  }
-  
-  export default DetailComp;
+}
+
+export default DetailComp;
+
--- src/SearchComp.jsx
+++ src/SearchComp.jsx
@@ -1,50 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import DetailComp from "./DetailComp";
 import "./Styling/CompStyles.css";
 
+//Skapar min Sökkomponent
 function SearchComp() {
-  const [searchMeal, setSearchMeal] = useState('');
-  const [mealResult, setMealResult] = useState([]);
-  const [selectedMealId, setSelectedMealId] = useState(null);
+  const inputSearch = useRef(null); //hänvisa till inputfältet
+  const [mealResult, setMealResult] = useState([]); //state för sökresultatet
+  const [clickedMealId, setClickedMealId] = useState(null); //state för när man klickat på en måltid
 
+  // Fånga upp värdet i sökrutan
   const handleInput = (e) => {
-    setSearchMeal(e.target.value);
+    const value = inputSearch.current.value;
   };
 
+  //eventhandler för sökknappen
   const handleBtn = (e) => {
     e.preventDefault();
     getMeal();
   };
 
+  //Funktion för att hämta datat från APIet
   const getMeal = () => {
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchMeal}`)
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputSearch.current.value}`)//matcha det som söks på med API
       .then((response) => response.json())
       .then((result) => {
-        setMealResult(result.meals || []);
+        setMealResult(result.meals);//lägg in API-resultatet i setMealResult
       })
       .catch((error) => {
-        console.error("Felmeddelande", error);
+        console.error("Felmeddelande", error); //Fånga upp felmeddelande
       });
   };
 
+  //funktion för specifikt maträtts-klick
   const handleMealClick = (id) => {
-    setSelectedMealId(id);
+    setClickedMealId(id);
   };
 
   return (
     <div>
+        {/*Sökformulär*/}
       <form className='searchForm' onSubmit={handleBtn}>
-        <input type="text" value={searchMeal} onChange={handleInput} />
-        <button type="submit">Sök</button>
+        <input ref={inputSearch} type="text" onChange={handleInput} />
+        <button type="submit">Söt</button>
       </form>
 
       <div>
         <ul>
-          {mealResult.map((meal) => (
-            <li key={meal.idMeal}>
+          {mealResult.map((meal) => ( //loopa genom listan av maträtter
+            <li key={meal.idMeal}> 
               <img src={meal.strMealThumb} alt={meal.strMeal}/>
-              <h2 className='clickMealName' onClick={() => handleMealClick(meal.idMeal)}>{meal.strMeal}</h2>
-              {meal.idMeal === selectedMealId && <DetailComp mealId={selectedMealId} />}
+              <h2 className='clickMealName' onClick={() => handleMealClick(meal.idMeal)}>{meal.strMeal}</h2> 
+              {/*infoga DetailComp för att hämta detaljer på den maträtt som klickas på*/}
+              {meal.idMeal === clickedMealId && <DetailComp mealId={clickedMealId} />}
             </li>
           ))}
         </ul>
